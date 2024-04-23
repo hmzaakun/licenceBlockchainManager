605eaf300e5fea78436b986d4aa84626972a2443
coralieBo <coralieboyer@Coralies-MacBook-Pro.local>
2024-04-23 17:08:10 +0200

    design

--- website/src/components/home/home.jsx
+++ website/src/components/home/home.jsx
@@ -9,32 +9,13 @@ import {
     animate,
 } from "framer-motion";
 
-const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+const Home = ({color, setShowSignup}) => {
 
-const Home = () => {
-    const color = useMotionValue(COLORS_TOP[0]);
-
-    useEffect(() => {
-        animate(color, COLORS_TOP, {
-            ease: "easeInOut",
-            duration: 10,
-            repeat: Infinity,
-            repeatType: "mirror",
-        });
-        // eslint-disable-next-line
-    }, []);
-
-    const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
     const border = useMotionTemplate`1px solid ${color}`;
     const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
-
+    
     return (
-        <motion.section
-            style={{
-                backgroundImage,
-            }}
-            className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
-        >
+        <div className="relative grid min-h-screen place-content-center px-4 py-24 text-gray-200 overflow-hidden">
             <div className="relative z-10 flex flex-col items-center">
                 <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
                     Accessing Digital Goods with NFT Licenses
@@ -53,6 +34,7 @@ const Home = () => {
                     whileTap={{
                         scale: 0.985,
                     }}
+                    onClick={() => setShowSignup(true)}
                     className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
                 >
                     Register now
@@ -65,7 +47,7 @@ const Home = () => {
                     <Stars radius={50} count={2500} factor={4} fade speed={2} />
                 </Canvas>
             </div>
-        </motion.section>
+        </div>
     );
 };
 
