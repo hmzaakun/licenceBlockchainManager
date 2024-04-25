9211ebb4b1410dd4b29e3303b6ce54026c4b5a94
coralieBo <coralieboyer@Coralies-MacBook-Pro.local>
2024-04-25 15:30:17 +0200

    moonpay

--- website/src/components/connexion/buyCrypto.tsx
+++ website/src/components/connexion/buyCrypto.tsx
@@ -1,12 +1,15 @@
 import { MoonPayBuyWidget } from '@moonpay/moonpay-react';
+import { useActiveAccount } from "thirdweb/react";
 
 const BuyWithMoonpay = ({ visible }: { visible: boolean }) => {
+    const account = useActiveAccount();
+    
     return (
         <MoonPayBuyWidget
             variant="overlay"
-            baseCurrencyCode="usd"
-            baseCurrencyAmount="100"
+            baseCurrencyCode="eur"
             defaultCurrencyCode="eth"
+            walletAddress={account?.address}
             visible={visible}
         />
     );
